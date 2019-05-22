34daf11b32713bc1293a6989f7713b40aa3d40e0
oz <oz.benoved@logmein.com>
2019-05-22 14:20:16 +0300

    added bot sample - featurs: missing entities, private information, contexts, in-app navigation, history

---
+++ botdemo/app/src/main/java/nanorep/com/botdemo/ChatType.java
@@ -0,0 +1,16 @@
+package nanorep.com.botdemo;
+
+import android.support.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static nanorep.com.botdemo.ChatType.LoggedIn;
+import static nanorep.com.botdemo.ChatType.NotLoggedIn;
+
+@Retention(RetentionPolicy.SOURCE)
+@StringDef({LoggedIn, NotLoggedIn})
+public @interface ChatType {
+    String LoggedIn = "loggedIn";
+    String NotLoggedIn = "notLoggedIn";
+}
